9bb3334372eb12cfff0f1c454d5616af0842bdb6
Tisks <leiser.mahu@usach.cl>
2021-02-16 21:38:51 -0300

    Adding cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors'
 //Routes
 import online_sensor from './routes/online_sensor';
 import sensor_endpoint from './routes/sensor_endpoint';
@@ -16,6 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors())
 
 //Routes
 app.use(online_sensor);
